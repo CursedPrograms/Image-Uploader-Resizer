12f8f063ebd3107de10e4e1428bc053a4a23fe68
Cursed Entertainment <110344485+CursedPrograms@users.noreply.github.com>
2023-12-05 18:50:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -49,16 +49,13 @@ def upload():
         if not os.path.exists(upload_folder):
             os.makedirs(upload_folder)
 
-        # Resize the image to 128x128
         resized_file_path = os.path.join(upload_folder, 'resized_' + file.filename)
         with Image.open(file) as img:
             img = img.resize((128, 128))
             img.save(resized_file_path)
-        
-        # Pause for 3 seconds
+            
         time.sleep(3)
         
-        # Redirect back to the upload page
         return redirect(url_for('index'))
 
 
